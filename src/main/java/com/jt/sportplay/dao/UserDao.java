1deefdfe636262924851776eba228c307d81fd1f
JTR4JT <2458638823@qq.com>
2023-03-22 17:18:25 +0800

    用户管理

--- src/main/java/com/jt/sportplay/dao/UserDao.java
+++ src/main/java/com/jt/sportplay/dao/UserDao.java
@@ -4,6 +4,8 @@ import com.jt.sportplay.bean.User;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author BXO
  * @version 1.0
@@ -15,4 +17,6 @@ import org.springframework.stereotype.Repository;
 public interface UserDao {
 
     public User getUserByMassage(@Param("username") String username,@Param("password") String password);
+    public List<User> getAllUser(@Param("username")String username,@Param("pageStart")int PageStart,@Param("pageSize")String pageSize);
+    public int getUserCounts(@Param("username")String username);
 }
