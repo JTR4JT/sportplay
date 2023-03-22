f308e8008bb4c41235b2187b2db609bbcc02da73
JTR4JT <2458638823@qq.com>
2023-03-22 17:21:50 +0800

    用户管理

---
+++ src/main/java/com/jt/sportplay/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.jt.sportplay.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author BXO
+ * @version 1.0
+ * @description: 用户管理
+ * @date 2023/3/22 17:19
+ */
+@RestController
+public class UserController {
+}
